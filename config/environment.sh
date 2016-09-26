b7a9a0af7ecb95c274f740ac617f617932caab2b
Yskinator <yski2moreletters@gmail.com>
2016-09-26 20:48:29 +0300

    Renamed project folder.

--- config/environment.sh
+++ config/environment.sh
@@ -3,5 +3,5 @@
 # korvaa tämä omalla käyttäjätunnuksellasi
 USERNAME="villtann"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="mita_unohdin"
+PROJECT_FOLDER="muistilista"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
