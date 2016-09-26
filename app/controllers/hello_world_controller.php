3f8ad66906cd61021230ffe093f909fed5b33b08
Yskinator <yski2moreletters@gmail.com>
2016-09-26 21:31:38 +0300

    Added plans for the main checklist page.

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -3,12 +3,17 @@
   class HelloWorldController extends BaseController{
 
     public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        View::make('home.html');
+        //echo 'Hello World!';
     }
-
+        
     public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+        // Testaa koodiasi täällä
+        //echo 'Hello World!';
+        View::make('helloworld.html');
+    }
+    public static function checklistplan(){
+        View::make('suunnitelmat/checklist.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,7 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+  $routes->get('/checklistplan', function() {
+    HelloWorldController::checklistplan();
+  });
