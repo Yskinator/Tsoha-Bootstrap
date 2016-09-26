317b722a7217bef6a0f87c4a258ca14ee8bc085f
Yskinator <yski2moreletters@gmail.com>
2016-09-26 22:14:22 +0300

    Added database table creation and removal.

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,28 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE CATEGORY(
+    id SERIAL PRIMARY KEY,
+    category_name varchar(200) NOT NULL,
+    supercategory INTEGER REFERENCES CATEGORY(id)
+);
+
+CREATE TABLE Users(
+    id SERIAL PRIMARY KEY,
+    username varchar(40) NOT NULL,
+    password varchar(40) NOT NULL,
+    list_root INTEGER REFERENCES CATEGORY(id)
+);
+
+CREATE TABLE NOTE(
+    id SERIAL PRIMARY KEY,
+    note varchar(200) NOT NULL,
+    supercategory INTEGER REFERENCES CATEGORY(id)
+);
+
+CREATE TABLE TIME_AND_PLACE(
+    id SERIAL PRIMARY KEY,
+    dow varchar(2),
+    tp_date DATE,
+    start_time TIME,
+    end_time TIME,
+    location varchar(200),
+    supercategory INTEGER REFERENCES CATEGORY(id)
+);
