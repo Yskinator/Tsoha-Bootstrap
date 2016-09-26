4c329eed9736af489d562910044170c9585daf02
Yskinator <yski2moreletters@gmail.com>
2016-09-26 23:06:00 +0300

    Added some TIME_AND_PLACE test data.

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,2 +1,4 @@
 -- USERS-taulun testidata
 INSERT INTO USERS (username, password) VALUES ('Ville-Matti', 'Ville-Matti123');
+-- TIME_AND_PLACE-taulun testidata
+INSERT INTO TIME_AND_PLACE(dow, tp_date, start_time, end_time, location) VALUES ('ti', '2011-11-11', '12:00', '14:00', 'Exactum');
