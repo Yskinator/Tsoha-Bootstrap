7c74a60437de3e311635a06d803ec2726635b75f
Yskinator <yski2moreletters@gmail.com>
2016-09-26 23:40:30 +0300

    Added a model for Category.

---
+++ app/models/Category.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Category
+ *
+ * @author ville-matti
+ */
+class Category extends BaseModel{
+    public $id, $category_name, $supercategory;
+
+    public function __construct($attributes){
+        parent::__construct($attributes);
+    }
+    
+    public static function all(){
+        $query = DB::connection()->prepare('SELECT * FROM CATEGORY');
+        $query->execute();
+        
+        $rows = $query->fetchAll();
+        $categories = array();
+        
+        foreach($rows as $row){
+            $categories[] = new Category(array(
+                'id' => $row['id'],
+                'category_name' => $row['category_name'],
+                'supercategory' => $row['supercategory']
+            ));
+        }
+        
+        return $categories;
+    }
+    
+    public static function find($id){
+        $query = DB::connection()->prepare('SELECT * FROM CATEGORY id = :id LIMIT 1');
+        $query->execute(array('id' => $id));
+        $row = $query->fetch();
+        
+        if($row){
+            $category = new Category(array(
+                'id' => $row['id'],
+                'category_name' => $row['category_name'],
+                'supercategory' => $row['supercategory']
+            ));
+            
+            return $category;
+        }
+        
+        return null;
+    }
+    
+    public function save(){
+        $query = DB::connection()->prepare('INSERT INTO CATEGORY (category_name, supercategory) VALUES (:category_name, :supercategory)');
+        $query->execute(array(
+            'category_name' => $this->category_name,
+            'supercategory' => $this->supercategory
+        ));
+        $row = $query->fetch();
+        $this->id = $row['id'];
+    }
+}
