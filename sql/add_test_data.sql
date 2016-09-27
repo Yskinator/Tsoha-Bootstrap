2b9d94bf1acb9bdb7e69821a9f41470ee62dcade
Yskinator <yski2moreletters@gmail.com>
2016-09-27 23:37:10 +0300

    Added CATEGORY test data.

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -2,3 +2,5 @@
 INSERT INTO USERS (username, password) VALUES ('Ville-Matti', 'Ville-Matti123');
 -- TIME_AND_PLACE-taulun testidata
 INSERT INTO TIME_AND_PLACE(dow, tp_date, start_time, end_time, location) VALUES ('ti', '2011-11-11', '12:00', '14:00', 'Exactum');
+--CATEGORY-taulun testidata
+INSERT INTO CATEGORY (id, category_name, supercategory) VALUES (1, '', NULL), (2, 'tsoha', 1), (3, 'tietokanta', 2), (4, 'matikka', 1)
